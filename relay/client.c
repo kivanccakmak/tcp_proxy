6664eaedcea60b0d7ce626752543c354d32f442e
Kivanc Cakmak <kivanccakmak@gmail.com>
2015-10-09 00:42:19 +0300

    repo cleaned
    Issue: TCP-1

--- relay/client.c
+++ relay/client.c
@@ -3,15 +3,49 @@
 */
 #include<stdio.h> //printf
 #include<string.h>    //strlen
+#include<stdlib.h>
 #include<sys/socket.h>    //socket
 #include<arpa/inet.h> //inet_addr
- 
+
+void execute(char *, char *);
+void write_some();
+void read_some();
+
 int main(int argc , char *argv[])
 {
+    /*int sock;*/
+    /*struct sockaddr_in server;*/
+    /*char message[1000] , server_reply[2000];*/
+    
+    if(argc != 3){
+        printf("wrong usage\n");
+        printf("%s ip_addr port\n", argv[0]);
+        printf("ex: %s 192.168.2.1 5050\n", argv[0]);
+        exit(EXIT_SUCCESS);
+    }else{
+        char * ip_addr;
+        printf("hello 1\n");
+        ip_addr = argv[1];
+        /*strcpy(ip_addr, argv[1]);*/
+        char * port;
+        printf("hello 2\n");
+        port = argv[2];
+        /*strcpy(port, argv[2]);*/
+        printf("hello 3\n");
+        execute(ip_addr, port); 
+    }
+    /*write_some();*/
+    read_some();
+    return 0;
+}
+
+void execute(char * ip_addr, char * port){
     int sock;
     struct sockaddr_in server;
     char message[1000] , server_reply[2000];
-     
+    printf("hello\n");
+    printf("ip: %s\n", ip_addr);
+    printf("port: %s\n", port);
     //Create socket
     sock = socket(AF_INET , SOCK_STREAM , 0);
     if (sock == -1)
@@ -21,25 +55,28 @@ int main(int argc , char *argv[])
     
     /*puts("Socket created");*/
      
-    server.sin_addr.s_addr = inet_addr(argv[1]);
+    server.sin_addr.s_addr = inet_addr(ip_addr);
     server.sin_family = AF_INET;
-    server.sin_port = htons( atoi(argv[2]) );
+    server.sin_port = htons( atoi(port) );
  
     //Connect to remote server
     if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
     {
         perror("connect failed. Error");
-        return 1;
+        exit(EXIT_SUCCESS);
+        /*return 1;*/
     }
      
     /*puts("Connected\n");*/
 
     int result = 0;
-    strcpy(message, argv[3]);
+    /*strcpy(message, argv[3]);*/
+    strcpy(message, "hello");
     result = send(sock, message, strlen(message), 0);
     if(result < 0){
         puts("Send failed");
-        return 1;
+        exit(EXIT_SUCCESS);
+        /*return 1;*/
     }else{
         printf("%d \n", result);
     }
@@ -47,11 +84,40 @@ int main(int argc , char *argv[])
     result = recv(sock, server_reply, 2000, 0);
     if(result < 0){
         puts("Receive failed");
-        return 1;
+        exit(EXIT_SUCCESS);
+        /*return 1;*/
     }
 
     puts(server_reply);
      
     close(sock);
-    return 0;
 }
+
+void write_some(){
+    FILE *fp;
+    fp = fopen("some.txt", "w+");
+    fprintf(fp, "hello fprintf ... \n");
+    fputs("shit \n", fp);
+    fclose(fp);
+}
+
+void read_some(){
+    FILE * fp;
+    char buff[255];
+
+    fp = fopen("some.txt", "r");
+    fscanf(fp, "%s", buff);
+    printf("1: %s\n", buff);
+
+    fscanf(fp, "%s", buff);
+    printf("2: %s\n", buff);
+
+    fscanf(fp, "%s", buff);
+    printf("3: %s\n", buff); 
+
+    fscanf(fp, "%s", buff);
+    printf("4: %s\n", buff); 
+
+    fclose(fp);
+}
+
