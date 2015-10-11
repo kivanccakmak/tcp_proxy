1e087742f763bab55f20f698ceeafa48cec6a67e
Kivanc Cakmak <kivanccakmak@gmail.com>
2015-10-11 11:28:16 +0300

    function created to read file

--- relay/file_read.c
+++ relay/file_read.c
@@ -4,6 +4,8 @@
 
 #define BLOCKSIZE 2 
 
+void read_file(FILE *fp, int start_byte, int block_number, char *buffer);
+
 int main(int argc, char *argv[]){
     if(argc != 4){
         printf("Wrong Usage\n");
@@ -11,20 +13,29 @@ int main(int argc, char *argv[]){
         printf("ex: %s kivanc.txt 10 100\n", argv[0]); 
         return 2;
     }
-    char *fname;
-    fname = argv[1];
+    char *fname = argv[1];
     int start_byte = atoi(argv[2]);
     int block_number = atoi(argv[3]);
+    char *buffer = (char *) malloc(block_number * BLOCKSIZE);
+    memset(buffer, '\0', block_number * BLOCKSIZE);
     FILE *fp;
-    char *buffer;
-    buffer = (char *) malloc(block_number * BLOCKSIZE);
     fp = fopen(fname, "a+");
+    read_file(fp, start_byte, block_number, buffer);
+    printf("%s\n", buffer);
+    memset(buffer, '\0', block_number * BLOCKSIZE);
+    printf("====\n");
+    start_byte += strlen(buffer);
+    read_file(fp, start_byte, block_number, buffer);
+    printf("%s\n", buffer);
+    free(buffer);
+    fclose(fp);
+}
+
+void read_file(FILE *fp, int start_byte, int block_number, char *buffer){
     size_t flen;
     size_t r_size = BLOCKSIZE;
     size_t nmemb = block_number;
     int fseek_result;
     fseek_result = fseek(fp, start_byte, SEEK_CUR);
     flen = fread(buffer, r_size, nmemb, fp);  
-    printf("%s\n", buffer);
-    fclose(fp);
 }
