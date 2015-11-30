0f65c1e0a5898c5dc5f46e46b6558c479522e693
Kivanc Cakmak <kivanc.cakmak@airties.com>
2015-11-30 12:50:04 +0200

    initial commit on netfilter split trials

---
+++ netfilter/src/nfqueue.h
@@ -0,0 +1,33 @@
+#ifndef NFQUEUE_H
+#define NFQUEUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/user.h>
+
+#include <netinet/in.h>
+#include <netinet/ether.h>
+#include <linux/netfilter.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+
+#include <errno.h>
+
+#include <net/if.h>
+#include <linux/if_ether.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#include <libnetfilter_queue/libnetfilter_queue.h>
+
+struct ipv4_packet{
+    struct iphdr iph;
+    struct tcphdr tcph;
+};
+
+#define QUEUER_BUF_SIZE PAGE_SIZE
+
+#endif 
